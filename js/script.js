499e930ca4e2b8ba04455a05480b2b121718e096
antor10655gh <creativefreelancerantor@gmail.com>
2022-01-25 12:52:26 +0600

    add all section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-//--------------navbar--------------//
 let navbar = document.querySelector('.header .navbar');
 let menuBtn = document.querySelector('#menu-btn');
 
@@ -12,8 +11,6 @@ window.onscroll = () =>{
    navbar.classList.remove('active');
 };
 
-
-//-------------------home-slider------------------//
 var swiper = new Swiper(".home-slider", {
    grabCursor:true,
    loop:true,
@@ -24,42 +21,82 @@ var swiper = new Swiper(".home-slider", {
    },
 });
 
-
-// --------------------food-slider--------------- //
 var swiper = new Swiper(".food-slider", {
-    grabCursor:true,
-    loop:true,
-    centeredSlides:true,
-    spaceBetween: 20,
-    pagination: {
-       el: ".swiper-pagination",
-       clickable: true,
-    },
-    breakpoints: {
-       0: {
-         slidesPerView: 1,
-       },
-       700: {
-         slidesPerView: 2,
-       },
-       1000: {
-         slidesPerView: 3,
-       },
-    },
- });
- 
- let previewContainer = document.querySelector('.food-preview-container');
- let previewBox = previewContainer.querySelectorAll('.food-preview');
- 
- document.querySelectorAll('.food .slide').forEach(food =>{
-    food.onclick = () =>{
-       previewContainer.style.display = 'flex';
-       let name = food.getAttribute('data-name');
-       previewBox.forEach(preveiw =>{
-          let target = preveiw.getAttribute('data-target');
-          if(name == target){
-             preveiw.classList.add('active');
-          }
-       });
-    };
- });
+   grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+let previewContainer = document.querySelector('.food-preview-container');
+let previewBox = previewContainer.querySelectorAll('.food-preview');
+
+document.querySelectorAll('.food .slide').forEach(food =>{
+   food.onclick = () =>{
+      previewContainer.style.display = 'flex';
+      let name = food.getAttribute('data-name');
+      previewBox.forEach(preveiw =>{
+         let target = preveiw.getAttribute('data-target');
+         if(name == target){
+            preveiw.classList.add('active');
+         }
+      });
+   };
+});
+
+previewContainer.querySelector('#close-preview').onclick = () =>{
+   previewContainer.style.display = 'none';
+   previewBox.forEach(close =>{
+      close.classList.remove('active');
+   });
+};
+
+var swiper = new Swiper(".menu-slider", {
+   grabCursor:true,
+   loop:true,
+   autoHeight:true,
+   centeredSlides:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+   grabCursor:true,
+   loop:true,
+   centeredSlides:true,
+   autoHeight:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+   },
+});
